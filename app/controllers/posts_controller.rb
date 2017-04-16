ab769464cb54dff57b8214ad3b240deac66250fe
Jake Day Williams <jake@wildacademy.com>
2017-04-16 15:44:04 -0600

    link to url or show post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
+# app/controllers/posts_controller.rb
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
+  end
+
+  def show
   end
 
   def new
@@ -18,7 +24,12 @@ class PostsController < ApplicationController
 
   private
 
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:title,:url,:body)
   end
+
 end
